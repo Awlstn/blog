33edbe5e401942002fdd494b93da7a9179b3bdf2
Awlstn <116646091+Awlstn@users.noreply.github.com>
2025-07-30 17:59:03 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          {count} 번을 클릭하셨어요.
+          {count} 번을 클릭했어요!!!.
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
